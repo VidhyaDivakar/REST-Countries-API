0283f8a6026e8ad6a7b16c1062a5ec2b01860401
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-04-11 18:04:19 -0400

    Added files

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
-alert("this script is connected");
+
+
+const apiURL = "https://restcountries.com/v3.1/all?fields=name,capital,currencies"; // storing the API endpoint as a const variable
+
+export async function fetchAllCountries() {
+     try {
+        const response = await fetch(apiURL);
+        if(!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+    const countries = await response.json();
+console.log(countries);
+} catch (error) {
+    console.error("Error fetching countries: ", error);
+}
+}
+fetchAllCountries();
