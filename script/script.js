13eb2c8733b9d4614930b14d3ea5dbe84352d23c
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-04-12 15:43:39 -0400

    Added the dom elements and logi, removed the hardcorded testing elements from html

--- script/script.js
+++ script/script.js
@@ -24,9 +24,48 @@ countries.forEach(country => {
 }
 function createCountryCard(country) {
    const name= country.name.common;
-   const populaton = country.populaton;
+   const population = country.population;
    const region = country.region;
-   const captial = country.capital ? country.captial[0] : "N/A";
+   const capital = country.capital ? country.capital[0] : "N/A";
    const flag = country.flags.png;
+
+   const card = document.createElement("div");
+   card.classList.add("country-card");
+
+
+   const flagDiv = document.createElement("div");
+   flagDiv.classList.add("flag");
+
+   const flagImg = document.createElement("flagImg");
+   img.src = flag;
+   img.alt = `${name} flag`;
+
+   flagDiv.appendChild(flagImg);
+
+    const info = document.createElement("div");
+    info.classList.add("country-info");
+
+    const cardTitle = document.createElement("h2");
+    cardTitle.textContent = name;
+
+    const cardPopulation = document.createElement("p");
+    cardPopulation.innerHTML = `<strong>Population:</strong> ${population}`; 
+
+    const cardRegion = document.createElement("p");
+    cardRegion.innerHTML = `<strong>Region:</strong> ${region}`;
+
+    const cardCapital = document.createElement("p");
+    cardCapital.innerHTML = `<strong>Captial:</capital> ${capital}`
+
+    info.appendChild(cardTitle);
+    info.appendChild(cardPopulation);
+    info.appendChild(cardRegion);
+    info.appendChild(cardCapital);
+
+    card.appendChild(flagDiv);
+    card.appendChild(info);
+
+    grid.appendChild(card);
+
 }
 fetchAllCountries();
