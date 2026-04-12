fcad9f428d7be8cdafd6af345e3db048c0e61d68
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-04-12 15:15:41 -0400

    addign function to fetch data from Countries API - Initial Draft

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 
-
+const grid = document.querySelector(".countries-grid");
 
 const apiURL = "https://restcountries.com/v3.1/all?fields=common,population,region,capital,flags"; // storing the API endpoint as a const variable
+
+
 
 export async function fetchAllCountries() {
      try {
@@ -11,8 +13,20 @@ export async function fetchAllCountries() {
         }
     const countries = await response.json();
 console.log(countries);
+grid.innerHTML = "";
+countries.forEach(country => {
+    createCountryCard(country);
+    
+});
 } catch (error) {
     console.error("Error fetching countries: ", error);
 }
+}
+function createCountryCard(country) {
+   const name= country.name.common;
+   const populaton = country.populaton;
+   const region = country.region;
+   const captial = country.capital ? country.captial[0] : "N/A";
+   const flag = country.flags.png;
 }
 fetchAllCountries();
